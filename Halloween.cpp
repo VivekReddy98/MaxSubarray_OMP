213a76fc6c9df4728ddd3198ae31dfff45724fb1
VivekReddy98 <vivekreddykarri98@gmail.com>
2020-09-12 06:50:16 -0400

    OMP in Prog

--- Halloween.cpp
+++ Halloween.cpp
@@ -9,7 +9,7 @@
 #endif
 
 
-// If not specified Set threads to Number of Cores
+// If not specified, Set threads to Number of Cores
 #ifndef OMP_NUM_THREADS
 #define OMP_NUM_THREADS omp_get_num_procs()
 #endif
@@ -43,7 +43,8 @@ void print(const Result& ); // Print output
 // OMP Helper Function to find Prefix_sum in Paralell
 void prefix_sum(std::vector<uint>&, const Environ&);
 // OMP Helper Function to perform a binary search in Paralell
-Result binary_search(std::vector<uint>&, const Environ&);
+Result binary_search_omp(std::vector<uint>&, const Environ&);
+void search_helper(const std::vector<uint>& prefix_sums, Result& ans, const Environ& env);
 
 
 int main(int argc, char **argv){
@@ -70,7 +71,7 @@ Result find_seq(const Environ& env){
 
 // Helper Print Function
 void print(const Result& result){
-    if (result.left == result.right && result.left == -1){
+    if (result.right == -1 || result.left == -1){
        std::cout << "Don't go here" << std::endl;
     }
     else {
@@ -147,41 +148,85 @@ Result find_seqSerial(const Environ& env){
 /* O(num_homes*log(num_homes)) version parallelized*/
 Result find_seqOMP(const Environ& env){
     struct Result result;
-    // std::vector<uint> temp(env.num_homes, 0);
     std::vector<uint> temp = env.pieces;
     prefix_sum(temp, env);
-    return binary_search(temp, env);
+    return binary_search_omp(temp, env);
 }
 
+/* Simple Prefix Scan in Paralell in O((num_homes/NumThreads)*log(num_homes)) complexity */
+/* O(num_homes) space complexity */
 void prefix_sum(std::vector<uint>& temp, const Environ& env){
     size_t numthreads = OMP_NUM_THREADS;
-
     std::vector<uint> read_only(env.num_homes, 0);
-    for (int stride = 1; stride <= env.num_homes; stride*=2) {
+
+    for (size_t stride = 1; stride <= env.num_homes; stride*=2) {
 
         // Maintain a Temp Array to remove intra-array dependency
         #pragma omp parallel for shared(env, stride, temp) num_threads(numthreads)
-        for (int i = 0; i < env.num_homes; i++){
+        for (size_t i = 0; i < env.num_homes; i++){
             read_only[i] = temp[i];
         }
 
         #pragma omp parallel for shared(env, stride, temp) num_threads(numthreads)
-        for (int i = stride; i <= env.num_homes; i++) {
+        for (size_t i = stride; i < env.num_homes; i++) {
             temp[i] += read_only[i-stride];
         }
     }
-
-    #ifdef DEBUG
-      for (size_t i = 0; i < env.num_homes; i++){
-        std::cout << temp[i] << " ";
-      }
-      std::cout << std::endl;
-    #endif
 }
 
-Result binary_search(std::vector<uint>&, const Environ&){
+Result binary_search_omp(std::vector<uint>& prefix_sums, const Environ& env){
     struct Result result;
+    size_t numthreads = OMP_NUM_THREADS;
+
+    // #pragma omp parallel for num_threads(numthreads)
+    for (size_t i = 0; i < env.num_homes; i++){
+
+        struct Result thread_priv{0, (int)i, prefix_sums[i]};
+
+        if (env.pieces[i] > env.max_cnds){
+            continue;
+        }
+        else if (prefix_sums[i] > env.max_cnds){
+            search_helper(prefix_sums, thread_priv, env);
+        }
+
+        if (result.num_candies < thread_priv.num_candies ||
+           (result.num_candies == thread_priv.num_candies
+              && result.left > thread_priv.left && result.left != -1)){
+            #pragma omp critical
+            {
+                result = thread_priv;
+            }
+        }
+    }
     return result;
 }
-
 #endif
+
+void search_helper(const std::vector<uint>& prefix_sums, Result& ans, const Environ& env) {
+    int temp_left = ans.left;
+    int temp_right = ans.right;
+    int optim_left = -1, optim_val = 0;
+    while(temp_left <= temp_right){
+        int mid = temp_left + (temp_right - temp_left)/2;
+        uint sum_for_this_subarray = prefix_sums[ans.right];
+        if (mid > 0)  sum_for_this_subarray -= prefix_sums[mid-1];
+        if (sum_for_this_subarray > env.max_cnds){
+            if (temp_left == mid) break;
+            temp_left = mid;
+        }
+        else if (sum_for_this_subarray < env.max_cnds){
+            if (temp_right == mid) break;
+            temp_right = mid;
+            optim_val = sum_for_this_subarray;
+            optim_left = mid;
+        }
+        else{
+            optim_val = sum_for_this_subarray;
+            optim_left = mid;
+            break;
+        }
+    }
+    ans.left = optim_left;
+    ans.num_candies = optim_val;
+}
